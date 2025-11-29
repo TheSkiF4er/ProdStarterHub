c73182f294c5f98bf6aa24508e2e900033af598e
Dmitry Efimov <cajeerskif4er@gmail.com>
2025-11-29 14:31:38 +0300

    Create index.ts

---
+++ website/src/components/index.ts
@@ -0,0 +1,13 @@
+export * from "./layout/PrimaryLayout";
+export * from "./layout/SiteHeader";
+export * from "./layout/SiteFooter";
+
+export * from "./ui/Button";
+export * from "./ui/Badge";
+export * from "./ui/Card";
+export * from "./ui/Tag";
+export * from "./ui/Callout";
+
+export * from "./docs/DocsLayout";
+
+export * from "./templates/TemplateLegend";
